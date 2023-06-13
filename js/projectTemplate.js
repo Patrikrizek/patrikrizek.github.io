6bf98ff8c18b5fc308816233e0423cecda2be673
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2023-06-13 14:37:46 +0100

    Add a new project

--- js/projectTemplate.js
+++ js/projectTemplate.js
@@ -1,5 +1,15 @@
 const projects = [
     {
+        name: "Expense Tracker",
+        img: "/media/react-app-expense-tracker.JPG",
+        imgAlt: "React App Expense Tracker",
+        badge: "#react-app",
+        label: "react-app",
+        description: "This is a mini project of a simple expense tracker. You can add or delete your expenses, filter them and see the total amount you spent.",
+        technology: "React 18, TS, Bootstrap, zod, react-hook-form",
+        link: "https://64886dc16e114e00beedc7e6--lustrous-cucurucho-9cdac0.netlify.app/"
+    },
+    {
         name: "Taste of Monarchy",
         img: "/media/web-taste-of-monarchy.JPG",
         imgAlt: "Website Taste of Monarchy",
